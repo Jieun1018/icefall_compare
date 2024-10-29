c24213a5ea6b59b091aea1af2d0788783c3b1ad0
Jieun Choi <1018uni@gmail.com>
2024-10-29 23:55:42 +0900

    running script edit - max duration

--- egs/librispeech/ASR/run_conformer_ctc2.sh
+++ egs/librispeech/ASR/run_conformer_ctc2.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 --exp-dir conformer_ctc2/exp \
 --full-libri 1 \
 --spec-aug-time-warp-factor 80 \
---max-duration 300 \
+--max-duration 1200 \
 --world-size ${WORLD_SIZE} \
 --start-epoch 1 \
 --num-epochs 30 \
